1dbdee4ba413ddd0020bf700f8033cda1cf18ab1
christochow <christochow@yahoo.com.hk>
2019-12-02 14:45:15 -0500

    added instruction in main page and updated calc component messages

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Button} from "@material-ui/core";
-import { withRouter } from "react-router-dom";
+import {withRouter} from "react-router-dom";
 import {connect} from "react-redux";
 import CourseComponent from "../components/CourseComponent";
 
@@ -16,12 +16,14 @@ class MainPage extends Component {
 
     render() {
         return (
-            <div style={{padding:'2vh'}}>
+            <div style={{padding: '2vh'}}>
                 <div style={{height: '25vh'}}/>
                 <h2>I Want To Pass</h2>
+                <p>Calculate how much you need to get on the final exam to pass a course :)</p>
                 <Button color="primary" onClick={this.onClick}>Add a new course</Button>
                 <h3>Saved courses</h3>
-                {this.props.course.map((e,ind) => (<CourseComponent key={ind} course={e}/>))}
+                {this.props.course.map((e, ind) => (<CourseComponent key={ind} course={e}/>))}
+                {this.props.course.length === 0 && 'No saved course at the moment'}
             </div>
         )
     };
