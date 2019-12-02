2ad18f70d68fbfb0e6dd3a662ac3e9130a2157f4
Chow Lok Man Christopher <3nine@Chows-MacBook-Pro.local>
2019-12-02 00:30:38 -0500

    Updates

---
+++ precache-manifest.01329eb49995a9cf079cb01cb0a5ecef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37da02a292e2a00e4738ff4b4fce5e33",
+    "url": "/i-want-to-pass/index.html"
+  },
+  {
+    "revision": "2b49896877730a13e5ec",
+    "url": "/i-want-to-pass/static/css/main.16f6dfe4.chunk.css"
+  },
+  {
+    "revision": "9c6fde1e7827c34157a4",
+    "url": "/i-want-to-pass/static/js/2.e2477424.chunk.js"
+  },
+  {
+    "revision": "2b49896877730a13e5ec",
+    "url": "/i-want-to-pass/static/js/main.f0d9faa3.chunk.js"
+  },
+  {
+    "revision": "8cbd72332c65941de8d7",
+    "url": "/i-want-to-pass/static/js/runtime-main.a149467c.js"
+  }
+]);
