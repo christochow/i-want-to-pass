f1e0ac51643d356c98fddea22b65055e31903007
Chow Lok Man Christopher <3nine@Chows-MacBook-Pro.local>
2019-12-02 01:02:03 -0500

    Updates

--- precache-manifest.251bb90d3f3734e8e2371a07ddfb41f7.js
+++ precache-manifest.251bb90d3f3734e8e2371a07ddfb41f7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69347c34a9205964e12f81ff4014467e",
+    "revision": "9a0da42333d977adfe2b91b191ac9e02",
     "url": "/i-want-to-pass/index.html"
   },
   {
-    "revision": "d694a589fdc5c7adf812",
+    "revision": "edce6bd8094acfbd6c74",
     "url": "/i-want-to-pass/static/css/main.16f6dfe4.chunk.css"
   },
   {
-    "revision": "9c6fde1e7827c34157a4",
-    "url": "/i-want-to-pass/static/js/2.e2477424.chunk.js"
+    "revision": "5eb96f0752accc75fd4b",
+    "url": "/i-want-to-pass/static/js/2.f34a9c64.chunk.js"
   },
   {
-    "revision": "d694a589fdc5c7adf812",
-    "url": "/i-want-to-pass/static/js/main.47ffa55e.chunk.js"
+    "revision": "edce6bd8094acfbd6c74",
+    "url": "/i-want-to-pass/static/js/main.ed9b25d6.chunk.js"
   },
   {
     "revision": "8cbd72332c65941de8d7",
