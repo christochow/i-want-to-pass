97aac1ef13689043a5b75bc0ff929fb7db9159f2
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2019-12-12 19:21:46 -0500

    added webview

--- android/app/src/main/java/com/christopherChow/iWantToPassAndroid/MainActivity.kt
+++ android/app/src/main/java/com/christopherChow/iWantToPassAndroid/MainActivity.kt
@@ -2,11 +2,24 @@ package com.christopherChow.iWantToPassAndroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        CookieManager.getInstance().acceptCookie();
+        webView.loadUrl("https://christochow.github.io/i-want-to-pass")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        CookieManager.getInstance().flush()
     }
 }
