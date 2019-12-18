477ed172e1867645a794fd6f0c55da5cfb397a7b
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2019-12-18 14:42:22 -0500

    make cookie banner appear over all pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,24 @@ import MainPage from "./pages/MainPage";
 import {Route, Switch} from "react-router-dom";
 import CalculatorPage from "./pages/CalculatorPage";
 import CookiePolicyPage from "./pages/CookiePolicyPage";
+import {isMobile} from "react-device-detect";
+import CookieBanner from "react-cookie-banner";
 
 function App() {
+
+    const cookieLink = (
+        <div style={{
+            verticalAlign: 'middle',
+            fontWeight: 400,
+            width: '100%',
+            lineHeight: '100px',
+            height:'100px'
+        }}>
+            This website uses cookies to store your entered data. By using IWantToPass, you agree to our <a
+            style={{textDecoration: 'underline'}} onClick={()=>this.props.history.push('/cookie')}>use of cookies</a>
+        </div>
+    );
+
     return (
         <div className="App">
             <Switch>
@@ -19,6 +35,14 @@ function App() {
                     <MainPage/>
                 </Route>
             </Switch>
+            {!isMobile && <div style={{position: 'absolute', bottom: 0, left: 0, width: '100%'}}>
+                <CookieBanner
+                    link={cookieLink}
+                    styles={{
+                        banner: { backgroundColor: 'rgba(60, 60, 60, 0.8)', height:'100px'},
+                    }}
+                    dismissOnScroll={false}/>
+            </div>}
         </div>
     );
 }
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -16,19 +16,6 @@ class MainPage extends Component {
         this.props.removeCourse(index)
     };
 
-    cookieLink = (
-        <div style={{
-            verticalAlign: 'middle',
-            fontWeight: 400,
-            width: '100%',
-            lineHeight: '100px',
-            height:'100px'
-        }}>
-           This website uses cookies to store your entered data. By using IWantToPass, you agree to our <a
-            style={{textDecoration: 'underline'}} onClick={()=>this.props.history.push('/cookie')}>use of cookies</a>
-        </div>
-    );
-
     render() {
         return (
             <div style={{padding: '2vh'}}>
@@ -40,14 +27,6 @@ class MainPage extends Component {
                 {this.props.course.map((e, ind) => (
                     <CourseComponent key={ind} callback={this.removeCallback(ind)} course={e}/>))}
                 {this.props.course.length === 0 && 'No saved course at the moment...'}
-                {!isMobile && <div style={{position: 'absolute', bottom: 0, left: 0, width: '100%'}}>
-                    <CookieBanner
-                                  link={this.cookieLink}
-                                  styles={{
-                                      banner: { backgroundColor: 'rgba(60, 60, 60, 0.8)', height:'100px'},
-                                  }}
-                                  dismissOnScroll={false}/>
-                </div>}
             </div>
         )
     };
