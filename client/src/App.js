05b306dd172575e28738e17b78f4a3ca91b9b926
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2019-12-19 10:14:43 -0500

    use custom path for webview and not display cookie banner there

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,23 @@ import MainPage from "./pages/MainPage";
 import {Route, Switch} from "react-router-dom";
 import CalculatorPage from "./pages/CalculatorPage";
 import CookiePolicyPage from "./pages/CookiePolicyPage";
-import {isMobile} from "react-device-detect";
 import CookieBanner from "react-cookie-banner";
+import {useHistory, useLocation} from "react-router-dom";
 
 function App() {
+    const history = useHistory();
+    const location = useLocation();
 
     const cookieLink = (
         <div style={{
             verticalAlign: 'middle',
             fontWeight: 400,
             width: '100%',
-            lineHeight: '100px',
-            height:'100px'
+            lineHeight: '65px',
+            height: '65px'
         }}>
-            This website uses cookies to store your entered data. By using IWantToPass, you agree to our <a
-            style={{textDecoration: 'underline'}} onClick={()=>this.props.history.push('/cookie')}>use of cookies</a>
+            This website uses cookies to store your entered data. By using I Want To Pass, you agree to our <a
+            style={{textDecoration: 'underline'}} onClick={() => history.push('/cookie')}>use of cookies</a>
         </div>
     );
 
@@ -31,15 +33,20 @@ function App() {
                 <Route path='/cookie'>
                     <CookiePolicyPage/>
                 </Route>
+                <Route path='/mobile'>
+                    <MainPage/>
+                </Route>
                 <Route path='/'>
                     <MainPage/>
                 </Route>
             </Switch>
-            {!isMobile && <div style={{position: 'absolute', bottom: 0, left: 0, width: '100%'}}>
+
+            {!(location.pathname === '/mobile')
+            && <div style={{position: 'absolute', bottom: 0, left: 0, width: '100%'}}>
                 <CookieBanner
                     link={cookieLink}
                     styles={{
-                        banner: { backgroundColor: 'rgba(60, 60, 60, 0.8)', height:'100px'},
+                        banner: {backgroundColor: 'rgba(60, 60, 60, 0.8)', height: '65px'},
                     }}
                     dismissOnScroll={false}/>
             </div>}
