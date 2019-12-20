381047782f513a32105416e6d0dad0ebbdfa85f9
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2019-12-20 00:38:06 -0500

    remove redundant acceptCookie call

--- android/app/src/main/java/com/christopherChow/iWantToPassAndroid/MainActivity.kt
+++ android/app/src/main/java/com/christopherChow/iWantToPassAndroid/MainActivity.kt
@@ -2,7 +2,6 @@ package com.christopherChow.iWantToPassAndroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -15,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         val webView: WebView = findViewById(R.id.webview)
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
-        CookieManager.getInstance().acceptCookie();
         webView.loadUrl("https://christochow.github.io/i-want-to-pass/#/mobile")
     }
 
